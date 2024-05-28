5444d8782b953fdb9dfd1c28b86e1a004c54d86a
themrsami <shamastabrez1313@gmail.com>
2024-05-28 18:21:41 +0500

    authentication added

--- Screens/firebaseconfig.js
+++ Screens/firebaseconfig.js
@@ -5,13 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8_tPobmWvDJF4RCjPRrDJxjdM2n9IadY",
-  authDomain: "lms-authentication-9625f.firebaseapp.com",
-  projectId: "lms-authentication-9625f",
-  storageBucket: "lms-authentication-9625f.appspot.com",
-  messagingSenderId: "169193685948",
-  appId: "1:169193685948:web:629b7b942cafea6515a1b9",
-  measurementId: "G-TDSTRSCH7M"
+
 };
 
 // Initialize Firebase
