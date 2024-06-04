e200e0e48d0d7575b674eca9c1de5f8e997d6a6a
Usama Nazir <shamastabrez1313@gmail.com>
2024-06-04 09:40:07 +0500

    Update firebaseconfig.js

--- Screens/firebaseconfig.js
+++ Screens/firebaseconfig.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8_tPobmWvDJF4RCjPRrDJxjdM2n9IadY",
-  authDomain: "lms-authentication-9625f.firebaseapp.com",
-  projectId: "lms-authentication-9625f",
-  storageBucket: "lms-authentication-9625f.appspot.com",
-  messagingSenderId: "169193685948",
-  appId: "1:169193685948:web:629b7b942cafea6515a1b9",
-  measurementId: "G-TDSTRSCH7M"
+
 };
 
 // Initialize Firebase
